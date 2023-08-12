c142dd94b8ec7811d3c6c00894d32b9c814b50c2
Rochus Keller <me@rochus-keller.ch>
2023-08-12 18:12:21 +0200

    added original Lua implementation for convenience

---
+++ Lua/Readme.md
@@ -0,0 +1,7 @@
+This is the original Lua implementation as downloaded from
+
+https://github.com/smarr/are-we-fast-yet/
+commit 770c6649ed8e by 2020-04-03
+accessed 2020-08-08
+
+There is only one modification in cd.lua line 906 to force inner_iterations to be 2 at least
---
+++ Lua/run_all.sh
@@ -0,0 +1,14 @@
+SOM=luajit
+
+
+#this is the original order from rebench.conf; the iteration count is original up to Havlak which is 1500 in the original
+Benchmarks=( DeltaBlue Richards Json Havlak CD	Bounce List Mandelbrot NBody  Permute Queens Sieve Storage Towers )
+Iterations=( 12000     100      100  10     250	1500   1500 500        250000 1000    1000   3000  1000    600     )
+
+for i in "${!Benchmarks[@]}"
+do
+	echo "running" ${Benchmarks[i]} "with" ${Iterations[i]} "iterations"
+	$SOM harness.lua ${Benchmarks[i]} ${Iterations[i]} >> run_lua.log
+done
+
+
